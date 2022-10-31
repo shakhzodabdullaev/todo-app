b79b897509cd033139a1db89a2a78e2116f6cf95
shakhzodabdullaev <shakhzod.6673@gmail.com>
2022-10-31 21:36:45 +0500

    31.10.2022

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,31 @@ package main
 import (
 	//"log"
 
+	"log"
+
 	"github.com/shakhzodabdullaev/todo-app"
 	"github.com/shakhzodabdullaev/todo-app/pkg/handler"
+	"github.com/shakhzodabdullaev/todo-app/pkg/repository"
+	"github.com/shakhzodabdullaev/todo-app/pkg/service"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-
+	if err := initConfig(); err != nil {
+		log.Fatal("Error initializing configs: ", err.Error())
+	}
+	//handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("localhost:8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		logrus.Fatalf("error : %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
